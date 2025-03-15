6fed69ce230d7192b0e5784a7558dd9e2a91d267
morena1001 <josueflores15.jf@gmail.com>
2025-03-15 14:38:25 -0500

    can extract all image links that are not nested

--- Scraper/finale_2/main.py
+++ Scraper/finale_2/main.py
@@ -1,4 +1,5 @@
 import time
+import math
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -15,10 +16,10 @@ from selenium.webdriver import ChromeOptions
 
 options = ChromeOptions ()
 # options.add_argument ("--headless=new")
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("window_size=1280,800")
-options.add_argument("--disable-popup-blocking")
-options.add_argument("--disable-save-password-bubble")
+options.add_argument ("--disable-blink-features=AutomationControlled")
+options.add_argument ("window_size=1280,800")
+options.add_argument ("--disable-popup-blocking")
+options.add_argument ("--disable-save-password-bubble")
 
 #email: 
 email = 'morena010.kv@gmail.com'
@@ -29,39 +30,73 @@ username = "morena_kv"
 
 driver = webdriver.Chrome (options=options, service=ChromeService (ChromeDriverManager ().install ()))
 driver.get ("https://x.com/i/flow/login")
-time.sleep(2)
+time.sleep (3)
 #LOGIN
-driver.find_element(By.XPATH,'//input').send_keys(username)
-driver.find_elements(By.XPATH, '//*[@role="button"]')[2].click()
-time.sleep(2)
-# if len(driver.find_elements(By.XPATH,'//*[name="password"]')) == 0:
-#     driver.find_element(By.XPATH,'//input').send_keys(email)
-#     time.sleep(1)
-#     driver.find_elements(By.XPATH, '//*[@role="button"]')[1].click()
-#     time.sleep(2)
-driver.find_elements(By.XPATH,'//input')[1].send_keys(password)
-driver.find_elements(By.XPATH, '//*[@role="button"]')[3].click()
-time.sleep(2)
-driver.get("https://x.com/sakakaorin/media")
+driver.find_element (By.XPATH,'//input').send_keys(username)
+driver.find_elements (By.XPATH, '//*[@role="button"]')[2].click()
+time.sleep (2)
+# if len (driver.find_elements (By.XPATH,'//*[name="password"]')) == 0:
+#     driver.find_element (By.XPATH,'//input').send_keys (email)
+#     time.sleep (1)
+#     driver.find_elements (By.XPATH, '//*[@role="button"]')[1].click ()
+#     time.sleep (2)
+driver.find_elements (By.XPATH,'//input')[1].send_keys (password)
+driver.find_elements (By.XPATH, '//*[@role="button"]')[3].click ()
+time.sleep (2)
+driver.get ("https://x.com/syakenosu/media")
+time.sleep (2)
 
-#GRAB CONTENT
-content = driver.page_source
-soup = BeautifulSoup (content, "html.parser")
-time.sleep(5)
-driver.quit ()
+inner = driver.find_element (By.XPATH, '//*[contains(text(),"photos & videos")]')
+numPages = math.ceil (int (inner.get_attribute ("innerHTML").split (" ")[0]) / 30)
+stopScrolling = 0
+while True:
+    stopScrolling += 1;
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)", "")
+    if stopScrolling > numPages:
+        break;
+    time.sleep (2)
 
-results = []
-def parse_image_urls (classes, location, source):
-    print (len (soup.find_all ()))
-    print (soup.find_all ())
-    for a in soup.find_all (attrs={"class": classes}):
-        name = a.find(location)
-        if name not in results:
-            results.append(name.get(source))
-parse_image_urls ("css-175oi2r r-1mlwlqe r-1udh08x r-417010 r-aqfbo4 r-agouwx r-1p0dtai r-1d2f490 r-u8s1d r-zchlnj r-ipm5af", "img", "src")
-df = pd.DataFrame ({"links": results})
+list = driver.find_elements (By.XPATH, '//img')
+links = []
+for item in list:
+    # ALSO GRAB EXTRA IMAGES HIDDEN INSIDE TWEETS
+    if item.get_attribute ("src").startswith ("https://pbs.twimg.com/media/"):
+        # print ((item.get_attribute ("class")) + "   " + str (item.get_attribute ("src").startswith ("https://pbs.twimg.com/media/")))
+        links.append (item.get_attribute ("src"))
+df = pd.DataFrame ({"links": links})
 df.to_csv ("links.csv", index=False, encoding="utf-8")
 print ("FINISHED")
+
+time.sleep (2)
+driver.quit ()
+
+
+
+
+
+
+
+
+# #GRAB CONTENT
+# content = driver.page_source
+# soup = BeautifulSoup (content, "html.parser")
+# time.sleep (5)
+# driver.quit ()
+
+# results = []
+# def parse_image_urls (classes, location, source):
+#     print (len (soup.find_all ()))
+#     print (soup.find_all ())
+#     for a in soup.find_all (attrs={"class": classes}):
+#         name = a.find(location)
+#         if name not in results:
+#             results.append(name.get(source))
+# parse_image_urls ("css-175oi2r r-1mlwlqe r-1udh08x r-417010 r-aqfbo4 r-agouwx r-1p0dtai r-1d2f490 r-u8s1d r-zchlnj r-ipm5af", "img", "src")
+# df = pd.DataFrame ({"links": results})
+# df.to_csv ("links.csv", index=False, encoding="utf-8")
+# print ("FINISHED")
+
+
 
 
 # def parse_image_urls (classes, location, source):
